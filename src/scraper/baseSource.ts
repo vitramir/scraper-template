e3cb0750cbbe43fe1b2e52d02832e1b701bb2971
Vitalii Valkov <vitramir@icloud.com>
2024-03-19 01:05:01 +0400

    Add comments

--- src/scraper/baseSource.ts
+++ src/scraper/baseSource.ts
@@ -4,19 +4,22 @@ import { InternalCategories } from './internalCategories';
 @Injectable()
 export abstract class Source {
   abstract listCategories(): Promise<
-    { categoryId: string; categoryName?: string }[]
+    { categoryId: string; categoryName?: string }[] // list of all categories from the resource
   >;
 
   abstract getCategoryInfo(
     categoryId: string,
-  ): Promise<{ firstPage: string; totalPages?: number }>;
+  ): Promise<{ firstPage: string; totalPages?: number }>; // First page in the category and total amount of pages in the category (for cursor and offest pagination)
 
   abstract getSearchPage(
     categoryId: string,
     page: string,
   ): Promise<{
-    products: { id: string; searchData: any }[];
-    nextPage?: string;
+    products: {
+      id: string;
+      searchData: any; // entire product object from the search API
+    }[];
+    nextPage?: string; // Next page. Usually page+1 but in case of cursor based bagination could be an identifier
   }>;
 
   abstract getProductPage(
@@ -24,17 +27,17 @@ export abstract class Source {
   ): Promise<{ product: { id: string; detailedData: any } }>;
 
   abstract getProductImages(product: {
-    searchData?: any;
-    detailedData?: any;
+    searchData?: any; // entire product object from the search API
+    detailedData?: any; // entire product object from the product API
   }): Promise<string[]>;
 
   abstract fetchImage(url: string): Promise<ArrayBuffer>;
 
   abstract getProductDetails(product: {
-    searchData?: any;
-    detailedData?: any;
+    searchData?: any; // entire product object from the search API
+    detailedData?: any; // entire product object from the product API
   }): Promise<{
-    categoryId: string;
-    internalCategoryId: keyof typeof InternalCategories;
+    categoryId: string; // categoryId in source website
+    internalCategoryId: keyof typeof InternalCategories; // CategoryId in out internal DB
   }>;
 }
